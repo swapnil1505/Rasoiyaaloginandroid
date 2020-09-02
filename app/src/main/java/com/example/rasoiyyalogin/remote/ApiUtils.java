400e242fee36a6b1f75208a2fbe830d5a8adf7bc
swapnil shukla <swapnil.shukla1508@gmail.com>
2020-09-02 13:49:54 +0530

    third commit

--- app/src/main/java/com/example/rasoiyyalogin/remote/ApiUtils.java
+++ app/src/main/java/com/example/rasoiyyalogin/remote/ApiUtils.java
@@ -1,7 +1,7 @@
 package com.example.rasoiyyalogin.remote;
 
 public class ApiUtils {
-    public static final String BASE_URL = "https://gorest.co.in/public-api ";
+    public static final String BASE_URL = "https://gorest.co.in/public-api/ ";
 
     public static UserService getUserService(){
         return RetrofitClient.getClient(BASE_URL).create(UserService.class);
